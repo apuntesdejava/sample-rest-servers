e26d90557675cd142f69fd98de39c6b267e72eae
Diego Silva <diego.silva@apuntesdejava.com>
2020-03-12 14:25:01 -0500

    thorntail

---
+++ sample-rest-thorntail/README.md
@@ -0,0 +1,13 @@
+# Thorntail
+## Build
+```
+mvn clean package
+```
+## Run
+```
+java -jar target\sample-rest-thorntail-thorntail.jar
+```
+or
+```
+mvn thorntail:run
+```
---
+++ sample-rest-thorntail/src/main/java/com/apuntesdejava/model/Person.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2020 diego.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.apuntesdejava.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+
+@Entity
+public class Person implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long personId;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime created;
+
+    public Person() {
+        created = LocalDateTime.now();
+    }
+
+    public Person(String firstName, String lastName, String email) {
+        this();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(long personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+}
